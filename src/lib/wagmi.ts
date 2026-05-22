c624e94d47e0722071919926bdecd047c84ccffc
Biltechie <you@example.com>
2026-05-22 23:26:36 +0100

    Fix: use Alchemy RPC to fix CORS error

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,10 +1,15 @@
 'use client'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { mainnet, sepolia } from 'wagmi/chains'
+import { http } from 'wagmi'
 
 export const wagmiConfig = getDefaultConfig({
-  appName:     'Mathematical Visions',
-  projectId:   'mathvisions', // get a real one from cloud.walletconnect.com
-  chains:      [sepolia, mainnet],
-  ssr:         true,
+  appName:    'Mathematical Visions',
+  projectId:  'mathvisions',
+  chains:     [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/tKaj84zp4O5dqb6O8RAjr'),
+    [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/tKaj84zp4O5dqb6O8RAjr'),
+  },
+  ssr: true,
 })
